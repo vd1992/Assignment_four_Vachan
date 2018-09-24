eaedc199319db120e600466cc45db121b46a0975
vd1992 <40485260+vd1992@users.noreply.github.com>
2018-09-24 14:49:01 -0600

    Working

--- other/scripts.js
+++ other/scripts.js
@@ -1,5 +1,5 @@
 // Hamburger menu functionality with toggle//
-function menu(){
+let menu=function(){
     if (document.getElementById("menu").style.display=="flex"){
         document.getElementById("menu").style.display="none";
         return;
@@ -9,4 +9,19 @@ function menu(){
     }
 }
 
+// Map building via Google Maps API
+let holder=document.getElementById("maphold");
+	let locater=function(){
+		let myfunc=function(location){
+        	let results=location.coords;
+			let imag=new Image();
+ 			imag.src="https://maps.googleapis.com/maps/api/staticmap?center=" + results.latitude + "," + results.longitude+"&zoom=16&size=850x500&key=AIzaSyBAACnxYrk7BaKlO9HLon7RpU5srEzAo_c";
+            holder.appendChild(imag);
+            console.log(location); 
+ 			}
+ 		let err=function(){
+  			holder.innerHTML="Failure";
+ 		}
+ 		navigator.geolocation.getCurrentPosition(myfunc,err);
+    }
 
