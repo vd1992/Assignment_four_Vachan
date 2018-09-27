2ac8fc4e60a6fe4c84326ded89ff41fd6a4bc409
vd1992 <40485260+vd1992@users.noreply.github.com>
2018-09-27 11:41:10 -0600

    api adjust

--- other/scripts.js
+++ other/scripts.js
@@ -18,10 +18,22 @@ let holder=document.getElementById("maphold");
  			imag.src="https://maps.googleapis.com/maps/api/staticmap?center=" + results.latitude + "," + results.longitude+"&zoom=16&size=850x500&key=AIzaSyBAACnxYrk7BaKlO9HLon7RpU5srEzAo_c";
             holder.appendChild(imag);
             console.log(location); 
- 			}
+ 		}
  		let err=function(){
   			holder.innerHTML="Failure";
  		}
  		navigator.geolocation.getCurrentPosition(myfunc,err);
     }
+
+// Weather Stuff
+let WeatherFetch=function(){
+    let url="http://api.openweathermap.org/data/2.5/weather?q=Calgary,ca&APPID=117188a5327d55d4bca1feec9be3ee49";
+    fetch(url).then(response => {
+          return response.json();
+        }).then(weatherJSON => {
+          let weatherhold=document.getElementById("weatherH");
+          weatherhold.innerHTML="Temperature in Calgary "+ weatherJSON.main.temp +" Kelvin, Condition: " + weatherJSON.weather[1].description;
+    })
+};
+
 
